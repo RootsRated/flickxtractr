74c66c1b5dc26b1fab969d8db6ad56fd753ae254
Scott BonAmi <sbonami@me.com>
2016-05-01 15:56:52 -0400

    Add capybara and poltergeist for page access

--- flickxtractr.gemspec
+++ flickxtractr.gemspec
@@ -21,5 +21,7 @@ Gem::Specification.new do |spec|
   spec.add_development_dependency "bundler", "~> 1.7"
   spec.add_development_dependency "rake", "~> 10.0"
 
+  spec.add_dependency "capybara"
   spec.add_dependency "sinatra"
+  spec.add_dependency "poltergeist"
 end
--- lib/flickxtractr/extractr.rb
+++ lib/flickxtractr/extractr.rb
@@ -1,9 +1,25 @@
+require 'capybara'
+require 'capybara/dsl'
+require 'capybara/poltergeist'
+
 module Flickxtractr
   class Extractr
+    include Capybara::DSL
+
     attr_reader :url
 
     def initialize(url)
       @url = url
+
+      initialize_capybara!
+    end
+
+    private
+
+    def initialize_capybara!
+      Capybara.current_driver = :poltergeist
+      Capybara.javascript_driver = :poltergeist
+      visit(url)
     end
   end
 end
