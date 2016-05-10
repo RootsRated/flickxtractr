981b5c1666046b5e09e43cfa9d209e1d75985327
Scott BonAmi <sbonami@me.com>
2016-05-09 21:38:36 -0400

    Add exec command for installing dependencies

--- lib/flickxtractr/cli.rb
+++ lib/flickxtractr/cli.rb
@@ -4,5 +4,12 @@ require 'thor'
 module Flickxtractr
   class CLI < Thor
     package_name 'Flickxtractr'
+
+    desc "install", "Install library dependencies"
+    def install
+      %w{ exiftool phantomjs }.each do |dependency|
+        `brew install #{dependency}`
+      end
+    end
   end
 end
